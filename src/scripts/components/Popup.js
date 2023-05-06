546f2ae838ab6530c89bb6663a75fc78edce367a
Darya Kolotilina <colotilina.d@yandex.ru>
2023-05-06 10:54:33 +0200

    'change popup.js'

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -6,15 +6,15 @@ export default class Popup {
 
   open() {
     this._popupElement.classList.add('popup_opened');
-    document.addEventListener('keydown',(evt) => this._handleEscClose(evt));
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
     this._popupElement.classList.remove('popup_opened');
-    document.removeEventListener('keydown',(evt) => this._handleEscClose(evt));
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
-  _handleEscClose(evt) {
+  _handleEscClose = (evt) => {
     if (evt.code == "Escape") {
       this.close()
     }
